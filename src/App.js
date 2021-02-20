0ed7ad168ae0de4c8a5a707f039e4d47ac06a190
lwakis <lwakis@yandex.ru>
2021-02-20 17:41:45 +0700

    fix position

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import ProblemsList from './components/ProblemsList';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <ProblemsList />
     </div>
   );
 }
---
+++ src/components/ProblemsList/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './problemsList.scss'
+const ProblemsList = () => {
+    return (
+        <div className="problems-block">
+            <button>Создать заявку</button>
+        </div>
+    );
+}
+
+export default ProblemsList;
